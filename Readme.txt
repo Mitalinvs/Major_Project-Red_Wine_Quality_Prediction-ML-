13fedc2baa5f9df36a5338525fac982935c5ca8d
Mitali Bansal <98804925+Mitalinvs@users.noreply.github.com>
2023-10-17 18:50:54 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,9 @@
+pip install streamlit
+pip install -U scikit-learn scipy matplotlib
+
+#Type in terminal: py -m streamlit run app.py
+
+Good Quality:
+7.3,0.65,0,1.2,0.065,15,21,0.9946,3.39,0.47,10
+Bad Quality:
+7.4,0.7,0,1.9,0.076,11,34,0.9978,3.51,0.56,9.4
---
+++ app.py
@@ -0,0 +1,49 @@
+#Type in terminal: py -m streamlit run app.py
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+#loading the dataset to a pandas Dataframe
+df = pd.read_csv('Wine_Quality.csv')
+# converting quality column, if quality is greater than or equal to 7 to be 1 else 0
+df['quality']= df['quality'].apply(lambda x: 1 if x>=7 else 0)
+df['quality'].head()
+
+from sklearn.preprocessing import MinMaxScaler
+scale = MinMaxScaler()
+data_scale = scale.fit_transform(df)
+data_scaled = pd.DataFrame(data_scale)
+data_scaled.columns = df.columns
+data_scaled.head()
+
+# Splitting the dataset into predictor variable(X) and target variable (y).
+X = df.iloc[:,:-1]
+y = df.iloc[:,-1]
+
+# Splitting predictor and target variable into training and testing datasets
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+
+from sklearn.ensemble import RandomForestClassifier
+rf_model = RandomForestClassifier()
+rf_model.fit(x_train,y_train)
+
+# printing the accuracy of training and testing data
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report,roc_curve,roc_auc_score
+y_pred_train = rf_model.predict(x_train)
+y_pred_test = rf_model.predict(x_test)
+print('Accuracy score of Random Forest with training dataset is : ', accuracy_score(y_pred_train,y_train))
+print('Accuracy score of Random Forest with testing dataset is : ', accuracy_score(y_pred_test,y_test))
+
+#Web app
+st.title("Red-Wine Quality Prediction Model")
+input_data = st.text_input('Enter comma-separated input features here')
+if st.button('Predict'):
+    input_data_np_array = np.asarray(input_data.split(','), dtype=float)
+    reshaped_input = input_data_np_array.reshape(1, -1)
+    prediction = rf_model.predict(reshaped_input)
+    if prediction[0] == 1:
+        st.write('Good Quality Red-Wine')
+    else:
+        st.write('Bad Quality Red-Wine')
